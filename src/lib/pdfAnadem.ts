dc08e9940df895372966392a7645e11fa92c63ce
Igo Matos <igomatos@hotmail.com>
2026-05-15 07:00:14 -0300

    Corrigir leitura PDF janeiro Anadem

--- src/lib/pdfAnadem.ts
+++ src/lib/pdfAnadem.ts
@@ -52,10 +52,16 @@ export async function lerPdfAnadem(file: File): Promise<RegistroPdfAnadem[]> {
 
   console.log('TEXTO COMPLETO DO PDF ANADEM:', texto);
 
+  const periodoMatch = texto.match(/Per[ií]odo de apura[cç][aã]o:\s*(\d{2})\/\d{2}\/(\d{4})/i);
+
+  const competenciaPadrao = periodoMatch
+    ? `${periodoMatch[1]}/${periodoMatch[2]}`
+    : '';
+
   const registros: RegistroPdfAnadem[] = [];
 
   const regex =
-    /(\d{3,6})\s*([A-Za-zÀ-ÿ0-9 .,&'()-]+?)\s+(\d{3}\.\d{3}\.\d{3}-\d{2}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2})\s+(\d{2}\/\d{2}\/\d{4})\s+R\$\s?([\d.,]+)\s+R\$\s?([\d.,]+)\s+(\d+%)\s+(\d{2}\/\d{4})\s+(\d{2}\/\d{2}\/\d{4})/g;
+    /(\d{3,6})\s*([A-Za-zÀ-ÿ0-9 .,&'()-]+?)\s+(\d{3}\.\d{3}\.\d{3}-\d{2}|\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2})\s+(\d{2}\/\d{2}\/\d{4})\s+R\$\s?([\d.,]+)\s+R\$\s?([\d.,]+)\s+(\d+%)\s+(\d{2}\/\d{2}\/\d{4})/g;
 
   let match;
 
@@ -68,8 +74,8 @@ export async function lerPdfAnadem(file: File): Promise<RegistroPdfAnadem[]> {
       comissao: converterMoeda(match[5]),
       baseCalculo: converterMoeda(match[6]),
       porcentagem: match[7],
-      competencia: match[8],
-      dataPagamento: match[9],
+      competencia: competenciaPadrao,
+      dataPagamento: match[8],
     });
   }
 
